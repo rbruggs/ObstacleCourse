a51e47e52e8dd9bfcdf0193cd7a06b3d59fa4c2b
rbruggs <rbruggs@gmail.com>
2021-09-23 14:28:04 -0400

    Uploading Files

---
+++ Obstacle Course/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Don't hit the walls or obstacles");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0, zValue);
+    }
+}
---
+++ Obstacle Course/Assets/Scripts/ObjectHit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Hit a wall");
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
